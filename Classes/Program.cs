f6b6a1a3fcf121cdfc5f9ef59674decc9151c523
Abdimajid <abdimajid254@gmail.com>
2024-07-20 19:08:27 -0400

    finished classes exercise

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var carOne = new Car();
+            carOne.Make = "Honda";
+            carOne.Model = "Accord";
+            carOne.year = 2021;
+            
+           Console.WriteLine($"This is a {carOne.Make} {carOne.Model}, made in the year{carOne.year}.");
         }
     }
 }
